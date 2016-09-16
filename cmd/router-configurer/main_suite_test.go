b3168b5257aa1e2d3888c03ae143515cdcaa5644
Swetha Repakula <srepaku@us.ibm.com>
2016-09-16 16:57:38 -0700

    wip wipwip
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- cmd/router-configurer/main_suite_test.go
+++ cmd/router-configurer/main_suite_test.go
@@ -10,9 +10,8 @@ import (
 	"code.cloudfoundry.org/cf-tcp-router/utils"
 	"code.cloudfoundry.org/consuladapter/consulrunner"
 	"code.cloudfoundry.org/routing-api"
+	"code.cloudfoundry.org/routing-api/cmd/routing-api/test_helpers"
 	routingtestrunner "code.cloudfoundry.org/routing-api/cmd/routing-api/testrunner"
-	"github.com/cloudfoundry/storeadapter"
-	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -29,17 +28,17 @@ var (
 	haproxyBaseConfigFile   string
 
 	consulRunner *consulrunner.ClusterRunner
+	dbAllocator  test_helpers.DbAllocator
 
-	etcdPort    int
-	etcdUrl     string
-	etcdRunner  *etcdstorerunner.ETCDClusterRunner
-	etcdAdapter storeadapter.StoreAdapter
+	etcdPort int
 
 	routingAPIAddress string
 	routingAPIArgs    routingtestrunner.Args
 	routingAPIPort    uint16
 	routingAPIIP      string
 	routingApiClient  routing_api.Client
+
+	dbEnv = os.Getenv("DB")
 )
 
 func TestRouterConfigurer(t *testing.T) {
@@ -98,12 +97,22 @@ defaults
 	Expect(err).ShouldNot(HaveOccurred())
 	Expect(utils.FileExists(haproxyConfigFile)).To(BeTrue())
 
-	etcdPort = 4001 + GinkgoParallelNode()
-	etcdUrl = fmt.Sprintf("http://127.0.0.1:%d", etcdPort)
-	etcdRunner = etcdstorerunner.NewETCDClusterRunner(etcdPort, 1, nil)
-	etcdRunner.Start()
+	switch dbEnv {
+	case "etcd":
+		port := 4001 + GinkgoParallelNode()
+		dbAllocator = test_helpers.NewEtcdAllocator(port)
+	default:
+		dbAllocator = test_helpers.NewMySQLAllocator()
+	}
+	dbId, err := dbAllocator.Create()
+	Expect(err).NotTo(HaveOccurred())
 
-	etcdAdapter = etcdRunner.Adapter(nil)
+	// test_helpers.
+	// 	etcdUrl = fmt.Sprintf("http://127.0.0.1:%d", etcdPort)
+	// etcdRunner = etcdstorerunner.NewETCDClusterRunner(etcdPort, 1, nil)
+	// etcdRunner.Start()
+
+	// etcdAdapter = etcdRunner.Adapter(nil)
 
 	routingAPIPort = uint16(6900 + GinkgoParallelNode())
 	routingAPIIP = "127.0.0.1"
@@ -112,7 +121,7 @@ defaults
 	routingAPIArgs = routingtestrunner.Args{
 		Port:       routingAPIPort,
 		IP:         routingAPIIP,
-		ConfigPath: createConfig(etcdUrl, consulRunner.URL()),
+		ConfigPath: createConfig(dbId, consulRunner.URL()),
 		DevMode:    true,
 	}
 	routingApiClient = routing_api.NewClient(routingAPIAddress, false)
@@ -135,10 +144,13 @@ var _ = SynchronizedAfterSuite(func() {
 	gexec.CleanupBuildArtifacts()
 })
 
-func createConfig(etcdUrl, consulUrl string) string {
+func createConfig(dbId, consulUrl string) string {
+	var configBytes []byte
 	configFilePath := fmt.Sprintf("/tmp/example_%d.yml", GinkgoParallelNode())
 
-	configStr := `log_guid: "my_logs"
+	switch dbEnv {
+	case "etcd":
+		etcdConfigStr := `log_guid: "my_logs"
 uaa_verification_key: "-----BEGIN PUBLIC KEY-----
 
       MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHFr+KICms+tuT1OXJwhCUmR2d
@@ -167,8 +179,45 @@ etcd:
   node_urls: ["%s"]
 consul_cluster:
   servers: "%s"`
+		configBytes = []byte(fmt.Sprintf(etcdConfigStr, dbId, consulUrl))
+	default:
+		mysqlConfigStr := `log_guid: "my_logs"
+uaa_verification_key: "-----BEGIN PUBLIC KEY-----
 
-	configBytes := []byte(fmt.Sprintf(configStr, etcdUrl, consulUrl))
+      MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHFr+KICms+tuT1OXJwhCUmR2d
+
+      KVy7psa8xzElSyzqx7oJyfJ1JZyOzToj9T5SfTIq396agbHJWVfYphNahvZ/7uMX
+
+      qHxf+ZH9BL1gk9Y6kCnbM5R60gfwjyW1/dQPjOzn9N394zd2FJoFHwdq9Qs0wBug
+
+      spULZVNRxq7veq/fzwIDAQAB
+
+      -----END PUBLIC KEY-----"
+
+debug_address: "1.2.3.4:1234"
+metron_config:
+  address: "1.2.3.4"
+  port: "4567"
+metrics_reporting_interval: "500ms"
+statsd_endpoint: "localhost:8125"
+statsd_client_flush_interval: "10ms"
+system_domain: "example.com"
+router_groups:
+- name: "default-tcp"
+  type: "tcp"
+  reservable_ports: "1024-65535"
+sqldb:
+  username: "root"
+  password: "password"
+  schema: "%s"
+  port: 3306
+  host: "localhost"
+  type: "mysql"
+consul_cluster:
+  servers: "%s"`
+		configBytes = []byte(fmt.Sprintf(mysqlConfigStr, dbId, consulUrl))
+	}
+
 	err := utils.WriteToFile(configBytes, configFilePath)
 	Expect(err).ShouldNot(HaveOccurred())
 	Expect(utils.FileExists(configFilePath)).To(BeTrue())
